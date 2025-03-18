a28401df852d1ee655bd30063ec36c3045d7ca9c
Kasidit Bualoeng <subdit@gmail.com>
2025-03-18 15:27:58 -0400

    create loader file and not-found page

--- thai-dress/app/(root)/page.tsx
+++ thai-dress/app/(root)/page.tsx
@@ -1,7 +1,10 @@
-// import { Button } from '@/components/ui/button';
+// import NotFoundPage from '../not-found';
+import { APP_NAME } from '../../lib/constants/index';
 
-const HomePage = () => {
-  return <div>Thai Dress</div>;
+const delay = (ms: 2000) => new Promise(resolve => setTimeout(resolve, ms));
+const HomePage = async () => {
+  await delay(2000);
+  return <div>{APP_NAME}</div>;
 };
 
 export default HomePage;
